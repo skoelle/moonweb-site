36ff6f97dde54fe277437334b00495f4085f7fa7
Stefan Koelle <stefan.koelle@gmail.com>
2026-08-17 00:05:31 +0200

    404 on stefankoelle.de

--- stefankoelle/eleventy.config.js
+++ stefankoelle/eleventy.config.js
@@ -4,7 +4,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "stefankoelle/assets": "assets" });
   eleventyConfig.addPassthroughCopy({ "stefankoelle/ledmatrix/assets": "ledmatrix/assets" });
   eleventyConfig.addPassthroughCopy({ "stefankoelle/robots.txt": "robots.txt" });
+  eleventyConfig.addPassthroughCopy({ "stefankoelle/.htaccess": ".htaccess" });
   eleventyConfig.addPassthroughCopy({ "stefankoelle/pdf/cv.pdf": "pdf/cv.pdf" });
+  eleventyConfig.addPassthroughCopy({ "shared/base.css": "shared-base.css" });
+  eleventyConfig.addPassthroughCopy({ "shared/theme-hub.css": "theme.css" });
+  eleventyConfig.addPassthroughCopy({ "shared/favicon/hub.svg": "favicon.svg" });
 
   return {
     dir: {
